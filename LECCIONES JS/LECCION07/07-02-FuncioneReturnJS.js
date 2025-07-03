ead8f60d9a60927865372375bdd007afb3343d0d
Andres <andres.nunez01@outlook.com>
2025-07-03 12:54:52 -0600

    Reorganizacion de archivos JS

--- LECCIONES JS/LECCION07/07-02-FuncioneReturnJS.js
+++ LECCIONES JS/LECCION07/07-02-FuncioneReturnJS.js
@@ -1,10 +1,8 @@
-miFuncion(4, 2);
-
 //Declaración de la función
 function miFuncion(a, b){
-    console.log("Suma: " + (a + b));   
+     return a + b;
 }
 
 //Llamando a la función
-miFuncion(2, 3);
-
+let resultado = miFuncion(2, 3);
+console.log(resultado);
